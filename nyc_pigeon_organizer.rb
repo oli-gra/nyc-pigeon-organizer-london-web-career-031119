5ac48efe758f40e8423875332484e88c01edf82c
Oliver <oliver.grant@gmail.com>
2019-02-01 13:23:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |stat_key,stats|
     stats.each do |stat,pigeons|
       pigeons.each do |pigeon|
         pigeon_list[pigeon] ||= {}
+        binding.pry
         pigeon_list[pigeon][stat_key] ||= []
         pigeon_list[pigeon][stat_key] << stat.to_s
       end
